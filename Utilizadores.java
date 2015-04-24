f370eb67768b2f6dd792de91132c49559abc5c1d
SusanaMendes <sukinha_mendes@hotmail.com>
2015-04-24 16:44:41 +0100

    !!!

--- Utilizadores.java
+++ Utilizadores.java
@@ -1,16 +1,13 @@
 import java.util.GregorianCalendar;
 import java.util.ArrayList;
 
-public class Utilizadores
-{
+public class Utilizadores extends Cache{
     private char genero;
     private String nome, email, password, morada;
     private GregorianCalendar datanascimento;
-    ArrayList<Caches> actividades;
+    ArrayList<Cache> actividades;
 	ArrayList<Utilizadores> redeamigos;	
 
 	
-    public Utilizadores(String nome, String email, String password, char genero, String morada, GregorianCalendar datanascimento)
-    {
     
     }
