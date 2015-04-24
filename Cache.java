6bcb73cd23e0108d64b49036ae7dd53471d1d38c
CesarMagalhaes <cesar_scp@hotmail.com>
2015-04-24 15:02:22 +0100

    Adicionar classes básicas

---
+++ Cache.java
@@ -0,0 +1,33 @@
+
+/**
+ * Escreva a descrição da classe Cache aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class Cache
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * COnstrutor para objetos da classe Cache
+     */
+    public Cache()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Exemplo de método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   exemplo de um parâmetro de método
+     * @return     a soma de x com y 
+     */
+    public int sampleMethod(int y)
+    {
+        // ponha seu código aqui
+        return x + y;
+    }
+}
