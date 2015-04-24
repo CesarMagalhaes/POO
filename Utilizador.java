bf0261a3fc8941aef197c7530dd7004281e1399d
SusanaMendes <sukinha_mendes@hotmail.com>
2015-04-24 16:31:04 +0100

    cenas..

---
+++ Utilizador.java
@@ -0,0 +1,16 @@
+import java.util.GregorianCalendar;
+import java.util.ArrayList;
+
+public class Utilizadores
+{
+    private char genero;
+    private String nome, email, password, morada;
+    private GregorianCalendar datanascimento;
+    ArrayList<Caches> actividades;
+	ArrayList<Utilizadores> redeamigos;	
+
+	
+    public Utilizadores(String nome, String email, String password, char genero, String morada, GregorianCalendar datanascimento)
+    {
+    
+    }
